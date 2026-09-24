087ad8906b7b8e9ceef3cdd1ec51cea575790eeb
Lee <lee.samchong@gmail.com>
2026-09-24 16:02:25 -0400

    login and registration server api routes

--- server/src/modules/auth/auth.routes.ts
+++ server/src/modules/auth/auth.routes.ts
@@ -1,37 +1,50 @@
 import {Router} from 'express'
 import { db } from '../../db'
-import { users } from '../../db/schema'
-import { eq, is } from 'drizzle-orm'
-import bcrypt from 'bcryptjs'
+import { eq } from 'drizzle-orm'
 import jwt from 'jsonwebtoken'
-import {set, z} from 'zod'
+import bcrypt from 'bcryptjs'
+import {httpUrl, z} from 'zod'
 
+import { users } from '../../db/schema'
 import { refreshTokens } from '../../db/schema'
+import { log } from 'node:console'
 
 const router = Router()
 
 const loginSchema = z.object({
-    username: z.string().min(3).max(30),
+    username: z.string().min(3).max(100),
     password: z.string().min(8)
 })
 
+const registerSchema = z.object({
+    firstName: z.string().min(3).max(50),
+    lastName: z.string().min(3).max(50),
+    username: z.string().min(3).max(50),
+    password: z.string().min(8),
+    confirmPassword: z.string()
+})
+.refine(
+    data => data.password === data.confirmPassword, {
+        message: 'Passwords dont match'
+    }
+)
+
 function issueAccessToken(userId: string){
     if(!process.env.ACCESS_TOKEN_SECRET){
-        throw new Error('ACCESS_TOKEN_SECRET not configured')
+        throw new Error('ACCESS_TOKEN_SECRET is not configured')
     }
 
-    return jwt.sign(userId, process.env.ACCESS_TOKEN_SECRET, {expiresIn: '30m'})
+    return jwt.sign({userId}, process.env.ACCESS_TOKEN_SECRET, {expiresIn: '30m'})
 }
-
 function issueRefreshToken(userId: string){
     if(!process.env.REFRESH_TOKEN_SECRET){
         throw new Error('REFRESH_TOKEN_SECRET is not configured')
     }
 
-    return jwt.sign(userId, process.env.REFRESH_TOKEN_SECRET, {expiresIn: '15d'})
+    return jwt.sign({userId}, process.env.REFRESH_TOKEN_SECRET, {expiresIn: '15d'})
 }
 
-function setAuthCookies(accessToken: string, refreshToken: string, res: any){
+function setAuthCookie(res: any, accessToken: string, refreshToken: string){
     const isProduction = process.env.NODE_ENV === 'production'
 
     const cookieOptions = {
@@ -52,7 +65,7 @@ function setAuthCookies(accessToken: string, refreshToken: string, res: any){
     })
 }
 
-function clearAuthCookies(res: any){
+function clearAuthCookie(res:any){
     const isProduction = process.env.NODE_ENV === 'production'
 
     const cookieOptions = {
@@ -70,20 +83,16 @@ router.post('/login', async (req, res) => {
     try{
         const data = loginSchema.parse(req.body)
 
-        if(!data){
-            return res.status(400).json({message: 'Need username and password'})
-        }
-
         const user = await db.query.users.findFirst({where: eq(users.username, data.username)})
 
         if(!user){
-            return res.status(401).json({message: 'Username or password invalid'})
+            return res.status(400).json({message: 'Username or password invalid'})
         }
 
-        const password = await bcrypt.compare(data.password, user.passwordHash) 
+        const validPassword = await bcrypt.compare(data.password, user.passwordHash)
 
-        if(!password){
-            return res.status(401).json({message: 'Username or password invalid'})
+        if(!validPassword){
+            return res.status(400).json({message: 'Username or password invalid'})
         }
 
         await db.delete(refreshTokens).where(eq(refreshTokens.userId, user.userId))
@@ -98,12 +107,58 @@ router.post('/login', async (req, res) => {
             revoked: false
         })
 
-        setAuthCookies(accessToken, refreshToken, res)
+        setAuthCookie(res, accessToken, refreshToken)
 
-        return res.status(200).json({message: 'Successfully logged in '})
+        return res.status(200).json({
+            message: 'Successfully logged in'
+        })
     }catch (error){
         console.error(error)
 
-        return res.status(500).json({message: 'Unable to login user'})
+        return res.status(500).json({message: 'Failed to log in user'})
+    }
+})
+
+router.post('/register', async (req, res) => {
+    try{
+        const data = registerSchema.parse(req.body)
+
+        const existingUser = await db.query.users.findFirst({where: eq(users.username, data.username)})
+
+        if(existingUser){
+            return res.status(409).json({message: 'Username already exist'})
+        }
+
+        const passwordHash = await bcrypt.hash(data.password, 12)
+
+        const [newUser] = await db.insert(users).values({
+            firstName: data.firstName,
+            lastName: data.lastName,
+            username: data.username,
+            passwordHash: passwordHash
+        }).returning({
+            userId: users.userId,
+            firstName: users.firstName,
+            lastName: users.lastName,
+            username: users.username
+        })
+
+        const accessToken = issueAccessToken(newUser.userId)
+        const refreshToken = issueRefreshToken(newUser.userId)
+
+        await db.insert(refreshTokens).values({
+            token: refreshToken,
+            userId: newUser.userId,
+            expiresAt: new Date(Date.now() + 15 * 24 * 60 * 60 * 1000),
+            revoked: false
+        })
+
+        setAuthCookie(res, accessToken, refreshToken)
+
+        return res.status(201).json({user: newUser})
+    }catch (error){
+        console.error(error)
+
+        return res.status(500).json({message: 'Failed to register user'})
     }
 })
